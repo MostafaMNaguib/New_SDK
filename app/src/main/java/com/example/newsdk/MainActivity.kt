daeb7433b0316a9758ba0590393cff4af4f6370b
mostafaMnaguib <mostafa.mohamad.naguib@gmail.com>
2022-03-07 16:47:44 +0200

    initial

--- app/src/main/java/com/example/newsdk/MainActivity.kt
+++ app/src/main/java/com/example/newsdk/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.newsdk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bitbang.first_sdk.ui.BaseActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val intent = Intent(this, BaseActivity::class.java)
+
+        intent.putExtra("Name","Test")
+        startActivity(intent)
+
+
     }
 }
